587ef93a2146b05bf0127a5cc9f66e1df3cf7d5c
hyperpikachu <lsj12323@naver.com>
2022-07-04 15:23:51 +0900

    movie project

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ movie.imdbID }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object,
+      default: () => ({})
+      //일반함수->default: function () {return {}}
+    }
+  }
+}
+</script>
